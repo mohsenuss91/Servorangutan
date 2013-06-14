184435151bf556a07dca3208a5674fbec421269c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 13:55:16 -0500

    edited index and single_survey.erb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,8 +11,14 @@ get '/surveys/:id' do
 end
 
 post '/submit_responses/:survey_id' do
-  Completion.create!(survey_id: params[:survey_id], user_id: [current_user])
-  params.values.each do |option_id|
-    Response.create!(user_id: [current_user], option_id: option_id)
+  Completion.create!(survey_id: params[:survey_id], user_id: 1)
+  params.values[0..-4].each do |option_id|
+  # above line is hackey, but this is because params[-3] is "splat"=>[], params[-2] is "captures"=>["3"], params[-1] is "survey_id"=>"3"
+    Response.create!(user_id: 1, option_id: option_id)
   end
+  redirect '/'
+end
+
+get '/surveys/:survey_id/responses' do
+  "Number of completions for survey " + params[:survey_id] + ": #{Survey.find(params[:survey_id]).completions.count}"
 end
